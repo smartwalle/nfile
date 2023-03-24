86cb302c7704a11f84964b91cd8be4c9c1221a61
smartwalle <smartwalle@gmail.com>
2023-03-24 10:22:16 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smartwalle/nfile
+
+go 1.18
---
+++ util.go
@@ -0,0 +1,56 @@
+package nfile
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// FileExists 检测文件是否存在
+func FileExists(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	if info.IsDir() {
+		return false, fmt.Errorf("the supplied path %s is a dir", path)
+	}
+	return true, nil
+}
+
+// DirExists 检测目录是否存在
+func DirExists(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	if !info.IsDir() {
+		return false, fmt.Errorf("the supplied path %s exists but is not a dir", path)
+	}
+	return true, nil
+}
+
+// DirEmpty 检测目录是否为空
+func DirEmpty(path string) (bool, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.ReadDir(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err
+}
